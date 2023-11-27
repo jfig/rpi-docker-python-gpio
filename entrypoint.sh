89a0c862d2469db7ccab39fa64edc925730898ea
Joao Figueiredo <joao.figueiredo@jfig.net>
2023-11-27 15:46:40 +0000

    first-blood entrypoint to install python packages

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Path to the marker file
+MARKER_FILE="/app/.first-run-complete"
+
+# Check if the marker file exists
+if [ ! -f "$MARKER_FILE" ]; then
+    # The marker file doesn't exist, so this is the first run
+
+    # Your commands go here
+    echo "Running first-time setup..."
+
+    pip install --no-cache-dir -r requirements.txt
+
+    apt-get purge -y \
+        gcc \
+        && apt-get autoremove -y
+
+    # Create the marker file to indicate first run is complete
+    touch "$MARKER_FILE"
+else
+    echo "This is not the first run. Skipping setup..."
+fi
+
+# Continue with the rest of your script
+python $APP
